87d60cf5c9583f2826040da90878049b964894d9
Angelo Zuniga <angeloeboy10@gmail.com>
2021-03-22 10:14:38 +0800

    edited login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
+        <div class="inner">
         <h1>Login</h1>
         <form>
+            <p>Username</p>
             <input type="text" v-model="userName">
+            <p>Password</p>
+
             <input type="password" v-model="passWord">
             <button @click="login">Login</button>
         </form>
+        </div>
+
 
     </div>
 </template>
@@ -37,8 +43,36 @@ export default {
 
 
 <style scoped>
+
+div{
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+}
+
+div.inner{
+    /* position: absolute;
+    top: 50%;
+    left: 50%;
+    height: 50%;
+    padding: 10px;
+    background-color: pink; */
+    display: block;
+    margin: 0 auto;
+    width: 30%;
+
+}
+
+input{
+    width: 100%;
+    display: block;
+    margin: 10px 0px;
+}
 button{
     background-color: black;
     color: white;
+    padding: 10px 30px;
+    border: 0px;
 }
 </style>
